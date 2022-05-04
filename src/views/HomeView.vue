238220101e01ab940cb3f787d4701ef692745d01
joemcbroom <joe.mcbroom.10@gmail.com>
2022-05-04 15:21:28 -0400

    Add sign in link to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,19 @@
 <script setup>
-import AuthWidget from '@/components/AuthWidget.vue';
+import DefaultButton from '../components/DefaultButton.vue';
 import { useAuthStore } from '@/stores/auth';
+import { RouterLink } from 'vue-router';
 
 const { supabase } = useAuthStore();
 </script>
 <template>
   <h1 class="text-center text-3xl mb-4">OG 1079 Reunion</h1>
   <h2 class="text-center text-2xl mb-4">Welcome to the site!</h2>
-  <p class="text-center text-lg">
+  <p class="text-center text-lg mb-4">
     We'll use this to organize OG 1079 Reunion events.
   </p>
-  <AuthWidget v-if="!supabase.auth.user()" />
+  <router-link to="/auth" v-if="!supabase.auth.currentSession">
+    <a class="text-ogGreen text-bold text-lg"
+      >Click Here to Sign In or Create Account</a
+    >
+  </router-link>
 </template>
